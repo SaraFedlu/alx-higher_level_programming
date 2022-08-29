9f3196274e02373a05e6abbf2bbfa67ac8ef9c21
Sara F <sfedlu28@gmail.com>
2022-08-29 10:59:05 +0000

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,7 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         return (0, 'none')
     else:
-        return (len(sentence), sentence[0])
+        for i in sentence:
+            if i != ' ':
+                break
+        return (len(sentence), i)
