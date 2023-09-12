ceb567a44e9f88bddedce3ea063c2703b8ee4e2c
Sara Fedlu <sfedlu28@gmail.com>
2023-09-12 13:28:09 -0700

    fix some error

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,9 +10,7 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        console.log('x');
-      }
+      console.log('X'.repeat(this.width));
     }
   }
 };
