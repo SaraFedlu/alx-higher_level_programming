9eb8b9804ca50981fd045c34fb2ac87c18272d30
Sara Fedlu <sfedlu28@gmail.com>
2023-09-12 15:00:26 -0700

    correct minor mistake

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const parentSq = require('./5-square');
 
 module.exports = class Square extends parentSq {
   charPrint (c) {
-    if (c === 'c' || c === 'C') {
+    if (c !== undefined) {
       for (let i = 0; i > super.size; i++) {
         console.log(c.repeat(super.size));
       }
