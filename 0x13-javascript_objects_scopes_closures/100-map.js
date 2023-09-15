62f6d68f61de93b159f48d3e0826932a60bb5610
Sara Fedlu <sfedlu28@gmail.com>
2023-09-15 13:46:29 -0700

    correct minor mistake in 100-map file

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const mapped = list.map((x) => x * list.indexOf(x));
+const mapped = list.map((item, index) => item * index));
 console.log(list);
 console.log(mapped);
