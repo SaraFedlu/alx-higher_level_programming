651fabb01af9f8a298092fed3e026c3d06ec4007
Sara Fedlu <sfedlu28@gmail.com>
2023-10-01 13:42:40 -0700

    add comment if it helps

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""A Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
