625e93006b6d83f3488e35887570a47e9e575174
Sara Fedlu <sfedlu28@gmail.com>
2023-10-01 08:53:13 -0700

    correct task #5 and #100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request and displays the status code of the response.
-curl -sI "$1" | grep 'HTTP' | cut -d' ' -f 2
+curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request and displays the body of the response.
-curl -s -d "email=test%40gmail.com" -d "subject=I+will+always+be+here_for+PLD" "$1"
+curl -s -d "email=test%40gmail.com&subject=I+will+always+be+here+for+PLD" "$1"
