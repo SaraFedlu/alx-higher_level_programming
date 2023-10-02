0896043104a7469054f8059aa3479c0af3040158
Sara Fedlu <sfedlu28@gmail.com>
2023-10-01 19:37:59 -0700

    fix indentation issue

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,6 @@ variable found in the header of the response.
 import urllib.request
 import sys
 if __name__ == "__main__":
-	req = urllib.request.Request(sys.argv[1])
-	with urllib.request.urlopen(req) as res:
-            print(dict(res.headers).get("X-Request-Id"))
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as res:
+        print(dict(res.headers).get("X-Request-Id"))
