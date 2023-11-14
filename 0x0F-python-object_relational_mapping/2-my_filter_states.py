f0a86e87cf7300dcd346428992ecd2a1c3a47d5a
Sara Fedlu <sfedlu28@gmail.com>
2023-11-14 07:30:20 -0800

    push third task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+python script that lists all states from the database hbtn_0e_0_usa with
+a given name
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
