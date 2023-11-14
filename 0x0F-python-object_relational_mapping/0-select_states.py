7939f33facfc6e8af93bf5b9383e9bf2251b8d17
Sara Fedlu <sfedlu28@gmail.com>
2023-11-14 07:15:47 -0800

    push second task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 '''
-   a script that lists all states 
+   a script that lists all states
    from the database hbtn_0e_0_usa
 '''
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', port=3306,\
+    db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
@@ -15,4 +15,3 @@ if __name__ == '__main__':
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
