3a09f8dbe866454b7538dcec60b552c6ea4b4bdb
Sara <105264543+SaraFedlu@users.noreply.github.com>
2023-11-14 17:40:04 +0300

    try my way

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 '''
-a script that lists all states from the database hbtn_0e_0_usa
+   a script that lists all states 
+   from the database hbtn_0e_0_usa
 '''
+import MySQLdb
+import sys
 
 if __name__ == '__main__':
-
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host='localhost', port=3306,
+    db = MySQLdb.connect(host='localhost', port=3306,\
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
@@ -17,19 +15,4 @@ if __name__ == '__main__':
     rows = cur.fetchall()
     for row in rows:
         print(row)
-=======
-'''
-lists all states from the database
-'''
-import MySQLdb
-from sys import argv
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
-    for i in range(num_rows):
-        print(cur.fetchone())
->>>>>>> origin/main
